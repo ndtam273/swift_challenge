29005f9dcac65fb54b7ae00a43d9618a481497be
tam nguyen <ndtam.273@gmail.com>
2020-11-26 18:12:57 +0700

    challenge 10

--- challenge.playground/Contents.swift
+++ challenge.playground/Contents.swift
@@ -15,9 +15,9 @@ let test2 = "No duplicates"
 let test3 = "Hello, world"
 
 func challenge1a(_ text: String) -> Bool {
-        for char in text {
+    for char in text {
         let charCount = text.components(separatedBy: String(char)).count - 1
-            if charCount > 1  {return false}
+        if charCount > 1  {return false}
     }
     return true
     
@@ -171,10 +171,10 @@ func challenge5a(_ text: String, _ char: Character) -> Int {
     var count = 0
     for letter in text.lowercased() {
         if letter == char {
-        count += 1
+            count += 1
+        }
+        
     }
-    
-}
     return count
 }
 
@@ -225,5 +225,132 @@ func challenge6b(_ text: String) -> String {
     return letters.joined()
 }
 challenge6b("Mississippi")
+
+//Challenge 7: Condense whitespace
+//Difficulty: Easy
+//Write a function that returns a string with any consecutive spaces replaced with a single space.
+//Sample input and output
+//I’ve marked spaces using “[space]” below for visual purposes:
+//• The string “a[space][space][space]b[space][space][space]c” should return “a[space]b[space]c”.
+//• The string “[space][space][space][space]a” should return “[space]a”.
+//• The string “abc” should return “abc”.
+
+func challenge7a(_ text: String) -> String {
+    let components = text.components(separatedBy: .whitespacesAndNewlines)
+    return components.filter{!$0.isEmpty}.joined(separator: " ")
+    
+}
+challenge7a("  a    b  c ")
+
+func challenge7b(_ text: String) -> String {
+    var seenSpace = false
+    var returnValue = ""
+    
+    for char in text {
+        if char != " " {
+            returnValue.append(char)
+            seenSpace = false
+        } else {
+            if !seenSpace {
+                returnValue.append(char)
+                seenSpace = true
+            } else {
+                continue
+                
+            }
+        }
+    }
+    return returnValue
+}
+
+challenge7b("  a    b  c ")
+
+func challenge7c(_ text: String) -> String {
+    return text.replacingOccurrences(of: " +", with: " ", options: .regularExpression, range: nil)
+}
+
+challenge7c("  a    b  c ")
+
+
+//Challenge 8: String is rotated
+//Difficulty: Tricky
+//Write a function that accepts two strings, and returns true if one string is rotation of the other,
+//taking letter case into account.
+//Tip: A string rotation is when you take a string, remove some letters from its end, then append them to the front. For example, “swift” rotated by two characters would be “ftswi”.
+//Sample input and output
+//• The string “abcde” and “eabcd” should return true.
+//• The string “abcde” and “cdeab” should return true.
+//• The string “abcde” and “abced” should return false; this is not a string rotation.
+//• The string “abc” and “a” should return false; this is not a string rotation.
+let textx = "cat"
+print(textx + textx)
+
+func challenge8a(_ text1: String, _ text2: String) -> Bool {
+    let doubleText1 = text1 + text1
+    return doubleText1.contains(text2) && doubleText1.count == text2.count * 2
+}
+challenge8a("abcde", "eabcd")
+challenge8a("abcde", "cdeab")
+challenge8a("abcde", "abced")
+challenge8a("abc", "a")
+
+
+//Challenge 9: Find pangrams
+//Difficulty: Tricky
+//Write a function that returns true if it is given a string that is an English pangram, ignoring
+//letter case.
+//Tip: A pangram is a string that contains every letter of the alphabet at least once.
+//Sample input and output
+//• The string “The quick brown fox jumps over the lazy dog” should return true.
+//• The string “The quick brown fox jumped over the lazy dog” should return false, because it’s missing the S.
+
+func challenge9a(_ text: String) -> Bool {
+    var alphabets : [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    for char in text.lowercased() {
+        guard let index = alphabets.firstIndex(of: char) else { continue }
+        alphabets.remove(at: index)
+    }
+    if alphabets.count == 0 { return true}
+    return false
+}
+
+challenge9a("The quick brown fox Jumps over the lazy dog")
+challenge9a("The quick brown fox jumped over the lazy dog")
+
+func challenge9b(_ text: String) -> Bool {
+    let set = Set(text.lowercased())
+    let letters = set.filter{ $0 >= "a" && $0 <= "z" }
+    return letters.count == 26
+}
+
+challenge9b("The quick brown fox Jumps over the lazy dog")
+challenge9b("The quick brown fox jumped over the lazy dog")
+
+
+//Challenge 10: Vowels and consonants
+//Difficulty: Tricky
+//Given a string in English, return a tuple containing the number of vowels and consonants.
+//Tip: Vowels are the letters, A, E, I, O, and U; consonants are the letters B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z.
+//Sample input and output
+//• The input “Swift Coding Challenges” should return 6 vowels and 15 consonants.
+//• The input “Mississippi” should return 4 vowels and 7 consonants.
+
+func challenge10a(_ text: String) -> (Int, Int) {
+    var vowelCount = 0
+    let noSpace = text.filter{ $0 != " "}
+    let vowels = "aeiou"
+    for char in text.lowercased() {
+        if vowels.contains(char) {
+            vowelCount += 1
+        }
+    }
+    return (vowelCount, noSpace.count - vowelCount)
+}
+
+print(challenge10a("Swift Coding Challenges"))
+print(challenge10a("Mississippi"))
+
+
+
 
 
